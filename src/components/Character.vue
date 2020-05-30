4ad8e7d0700c1a89fafef839189c85ee19ba5c81
rohmedina <ro.medina.gatica@gmail.com>
2020-05-29 23:39:32 -0400

    consumo de API establecido

---
+++ src/components/Character.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <img v-bind:src="character.image" v-bind:alt="character.name" />
+    </div>
+    <div>
+      <h3>{{ character.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["character"],
+};
+</script>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="inicio">
+    <h1>R&M <span>Personajes</span></h1>
+
+    <div class="card" v-for="character in characters" v-bind:key="character.id">
+      <img v-bind:src="character.image" v-bind:alt="character.name" />
+      <div class="card-body">
+        <h2 class="card-title">{{ character.name }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "inicio",
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  mounted() {
+    let vue = this;
+    axios.get("https://rickandmortyapi.com/api/character/").then(function(response) {
+      vue.characters = response.data.results;
+      console.log(vue.characters);
+    });
+  },
+};
+</script>
+
+<style>
+body {
+  background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.card {
+  display: flex;
+  align-items: center;
+  padding: 1em;
+}
+.card-title {
+  text-align: center;
+  padding: 30px;
+}
+h1,
+h2 {
+  color: rgb(68, 168, 68);
+}
+span {
+  color: white;
+}
+img {
+  border-radius: 50%;
+  width: 150px;
+  padding: 1em;
+}
+</style>
